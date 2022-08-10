9b6baf7cc8dff4cbca7dbf71bb4ea1b4d2fb2438
prem271220 <premkumarshankar271220@gmail.com>
2022-08-10 13:47:31 +0530

    third commit

--- MavenDemoGit/src/main/java/com/lti/demos/Hi.java
+++ MavenDemoGit/src/main/java/com/lti/demos/Hi.java
@@ -1,5 +1,19 @@
 package com.lti.demos;
 
 public class Hi {
+	
+	public static void main(String[] args) {
+		try {
+			int a=1,b=2;
+			a =a/(b-2);
+		}
+		catch(ArithmeticException ae) {
+			System.out.println("in catch");
+		}
+		finally {
+			System.out.println("in finally");
+		}
+	}
+	
 
 }
